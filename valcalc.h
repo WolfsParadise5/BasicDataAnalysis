e2ed19f176b7fb9f11bd232042e6d36cfb30628d
WolfsParadise5 <69388191+WolfsParadise5@users.noreply.github.com>
2020-08-29 16:20:27 +0800

    Update valcalc.h
    All functions only require vector

--- valcalc.h
+++ valcalc.h
@@ -14,75 +14,67 @@ int main() {
 }
 
 
-int minCalc(int valarr[100]) {
+///Source : https://www.bitdegree.org/learn/c-plus-plus-vector
+///Source : 
 
-    int lownum = valarr[0];
+#include<iostream>
+#include<vector>
 
-    for (i = 0; i < 100; i++)
-    {
-        if (valarr[i] < lownum)
-        {
-            lownum = valarr[i];
-        }
-    return lownum;
-    }
+using namespace std;
+
+int main() {
+
+    int mincalc(vector<int>& number);
+    int maxcalc(vector<int>& number);
+    int meancalc(vector<int>& number);
+    int mediancalc(vector<int>& number);
+    int variancecalc(vector<int>& number);
+    int standardDeviationCalc(vector<int>& number);
+}
+
+int minCalc(vector<int>& number) {
+
+    int min = *min_element(number.begin(),number.end());
+    return min;
+}
+
+int maxCalc(vector<int>& number) {
+
+    int max = *max_element(number.begin(),number.end());
+    return max;
 
 }
 
+int meanCalc(vector<int>& number)  {
+    int total = accumulate(number.begin(), number.end(),0);
+    int val = total / 100;
+    return total, val; 
+}
 
-int maxCalc(int valarr[100]) {
+int medianCalc(vector<int>& number) {
 
-    int highnum = valarr[0];
-
-    for (i = 0; i < 100; i++)
-    {
-        if (valarr[i] > highnum)
-        {
-            highnum = valarr[i];
-        }
-    return highnum;
-    }
-
+    size_t vecsize = number.size();
+    sort(number.begin(), number.end());
+    return (number[vecsize / 2 - 1] + number[size / 2]) / 2;
 
 }
 
+int varianceCalc(vector<int>& number) {
 
-int medianCalc(int valarr[100]) {
-
-    result = valarr[49];  //Between 0-99
-    return result;
-
-}
-
-
-int meanCalc(int valarr[100]) {
-
-    int total,result;
-
-    for(i = 0; i < 100; i++)
-    {
-        total += valarr[i];
-    }
-
-    total = num / 100;
-    return num, result;
+    int total = accumulate(number.begin(), number.end(),0);
+    int mean = total / 100;
+    int variance = (total - mean) ** 2 / 100;
+    return variance;
 
 }
 
+int standardDeviationCalc(vector<int>& number) {
 
-int varianceCalc(int total,int mean) {
-
-    int result = ((num - mean) ** 2 ) / 100;
-    return result;
-
-
-}
-
-
-int standardDeviationCalc(int variance) {
-
-    int result = variance ** 0.5;
-    return result;
+    int total = accumulate(number.begin(), number.end(),0);
+    int mean = total / 100;
+    int variance = (total - mean) ** 2 / 100;
+    int standardDeviation = variance ** 0.5;
+    return standardDeviation;
 
 }
 
