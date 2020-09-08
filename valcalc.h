f64a124db77f0c9059f70f9d0563ca4c9b50ad13
Naga K <nagak@pop-os.localdomain>
2020-09-08 23:22:59 +0800

    Removed duplicate codes

--- valcalc.h
+++ valcalc.h
@@ -1,24 +1,6 @@
-#include <iostream>
-using namespace std;
-
-int minCalc(int);
-int maxCalc(int);
-int medianCalc(int);
-int meanCalc(double);
-int varianceCalc(double);
-int standardDeviationCalc(double);
-
-int main() {
-
-
-}
-
-
 ///Source : https://www.bitdegree.org/learn/c-plus-plus-vector
-///Source : 
-
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -26,10 +8,10 @@ int main() {
 
     int mincalc(vector<int>& number);
     int maxcalc(vector<int>& number);
-    int meancalc(vector<int>& number);
-    int mediancalc(vector<int>& number);
-    int variancecalc(vector<int>& number);
-    int standardDeviationCalc(vector<int>& number);
+    float meancalc(vector<int>& number);
+    float mediancalc(vector<int>& number);
+    double variancecalc(vector<int>& number);
+    double standardDeviationCalc(vector<int>& number);
 }
 
 int minCalc(vector<int>& number) {
@@ -45,13 +27,13 @@ int maxCalc(vector<int>& number) {
 
 }
 
-int meanCalc(vector<int>& number)  {
+float meanCalc(vector<int>& number)  {
     double total = accumulate(number.begin(), number.end(),0);
     double val = total / 100;
-    return total, val; 
+    return total, val;
 }
 
-double medianCalc(vector<int>& number) {
+float medianCalc(vector<int>& number) {
 
     sort(number.begin(), number.end());
     return (number[100 / 2 - 1] + number[100 / 2]) / 2;
