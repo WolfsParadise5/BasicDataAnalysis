aa7586ce7c710d483c2f76367b94b9639a5730bb
jsliew <69413792+J-sensei@users.noreply.github.com>
2020-09-14 18:59:13 +0800

    Calculate Linear Regression Line

--- Linear.h
+++ Linear.h
@@ -8,19 +8,17 @@ int sum(const vector<int> &column);
 int sumMultiplication(const vector<int> &column1, const vector<int> &column2);
 double slope(const vector<int> &column1, const vector<int> &column2);
 double intercept(const vector<int> &column1, const vector<int> &column2, const double &slope);
-void displayTable(const vector<int> &column1, const vector<int> &column2, const string &subject1, const string &subject2, const int &width);
+void displayLinearTable(const vector<int> &column1, const vector<int> &column2, const string &subject1, const string &subject2, const int &width);
 void linearRegression(const vector<int> &column1, const vector<int> &column2, const string &subject1, const string &subject2);
 
 void linearRegression(const vector<int> &column1, const vector<int> &column2, const string &subject1, const string &subject2)
 {
     system("cls||clear");
 
-    cout << fixed << showpoint;
-    cout << setprecision(4);
     cout << "Column 1: " << subject1 << endl;
     cout << "Column 2: " << subject2 << endl;
 
-    displayTable(column1, column2, subject1, subject2, 12);
+    displayLinearTable(column1, column2, subject1, subject2, 12);
 
     double slopeNum = slope(column1, column2);
     double interceptNum = intercept(column1, column2, slopeNum);
@@ -32,27 +30,27 @@ void linearRegression(const vector<int> &column1, const vector<int> &column2, co
         cout << "Line Equation: y = " << slopeNum << "x + " << interceptNum << endl;
 }
 
-void displayTable(const vector<int> &column1, const vector<int> &column2, const string &subject1, const string &subject2, const int &width)
+void displayLinearTable(const vector<int> &column1, const vector<int> &column2, const string &subject1, const string &subject2, const int &width)
 {
     int numFields = 4;
     string seperator = " |";
     string result = "Sum: ";
     int totalWidth = (width * numFields) + seperator.size() * numFields;
     string line = seperator + string(totalWidth - 1, '-') + "|";
-
+    //display headers
     cout << line << endl;
     cout << seperator << setw(width) << subject1 + "/x" << seperator
         << setw(width) << subject2 + "/y" << seperator
         << setw(width) << "x * x" << seperator
         << setw(width) << "x * y" << seperator << endl;
     cout << line << endl;
-
+    //display values
     for(int i = 0; i < column1.size(); ++i)
         cout << seperator << setw(width) << column1[i] << seperator
             << setw(width) << column2[i] << seperator
             << setw(width) << column1[i]*column1[i] << seperator
             << setw(width) << column1[i]*column2[i] << seperator << endl;
-
+    //display result
     cout << line << endl;
     cout << seperator << result
         << setw(width - result.size()) <<sum(column1) << seperator
